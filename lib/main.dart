19422900b73a0df0019def9553b5463ce26035dc
nathanaelrpl1-cmyk <nathanael.rpl1@gmail.com>
2026-08-04 07:50:24 +0700

    LKPD-1 model data

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,35 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
-}
+  String NamaBarang = "Buku Gambar A3";
+  int HargaAnggota = 15000;
+  int HargaUmum = 20000;
+  int JumlahStok = 10;
+  bool StatusStok = true;
+  int JumlahBeli = 3;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  int TotalAnggota = HargaAnggota * JumlahBeli;
+  int TotalUmum = HargaUmum * JumlahBeli;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF1E88E5),
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Axel XII RPL 1'),
-    );
+  int selisihHarga = TotalUmum - TotalAnggota;
+
+  print("==== Kartu Anggota =====");
+  print("Nama Barang : $NamaBarang");
+  print("Harga Anggota : Rp$HargaAnggota");
+  print("Harga Umum : Rp$HargaUmum");
+  print("Jumlah Stok : $JumlahStok");
+  print("Sedang Tersedia : $StatusStok");
+  print("HargaTotal Anggota : Rp$TotalAnggota");
+  print("HargaTotal Umum : Rp$TotalUmum");
+  print("Selisih Harga : Rp$selisihHarga");
+  print("==================================");
   }
-}
+  
+// Pemilihan tipe data yang tepat sangat penting untuk mencegah 
+// kesalahan perhitungan matematis pada sistem kasir koperasi. 
+// Penggunaan tipe data angka seperti 'int' memastikan program 
+// dapat melakukan operasi hitung (seperti perkalian total belanja). 
+// Jika data seperti harga atau stok salah disimpan sebagai teks 
+// (String), aplikasi tidak akan bisa memproses kalkulasi transaksi 
+// secara otomatis dan memicu error.
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
