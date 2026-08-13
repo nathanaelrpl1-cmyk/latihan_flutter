da0f63907fef1b36331c9b9140eabf1db0957955
nathanaelrpl1-cmyk <nathanael.rpl1@gmail.com>
2026-08-13 13:23:22 +0700

    penggunaan try-catch

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,44 @@
-import 'package:intl/intl.dart';
 
 class Barang {
-  final formatRupiah = NumberFormat.currency(
-    locale: 'id_ID',
-    symbol: 'Rp',
-    decimalDigits: 2,
-  ); 
-
   String nama;
   double harga;
   int _stok;
 
-  Barang(this.nama, this.harga, this._stok);
-
-  int get stok => _stok;
+  Barang ({
+    required this.nama,
+    required this.harga,
+    required this._stok,
+  });
 
   bool jual(int n) {
     if (n > 0 && _stok >= n) {
       _stok -= n;
+      print("Transaksi Sukses. Berhasil menjual $n unit $nama. Sisa stok: $_stok");
       return true;
     } else {
-      print("Stok tidak mencukupi atau jumlah yang diminta tidak valid.");
+      print("Transaksi gagal. Stok tidak mencukupi atau jumlah yang diminta tidak valid.");
       return false;
     }
   }
-
-  void tampilkan() {
-    print("- $nama: ${formatRupiah.format(harga)} (Stok: $_stok)");
-  }
 }
 
-class BarangPromo extends Barang {
-  final formatRupiah = NumberFormat.currency(
-    locale: 'id_ID',
-    symbol: 'Rp',
-    decimalDigits: 2,
-  );
+void prosesBeli(String inputJumlah, Barang barang) {
+  print("Kasir menginput jumlah: '$inputJumlah'");
 
-  double Diskon;
-
-  BarangPromo(String nama, double harga, int stok, this.Diskon) 
-      : super(nama, harga, stok);
-
-  double HargaPromo() {
-    return harga - (harga * Diskon / 100);
+  try {
+    int jumlah = int.parse(inputJumlah);
+    barang.jual(jumlah);
+  } catch (error) {
+    print("Sepertinya input jumlah tidak valid. Harap massukan angka yang benar.");
+  } finally {
+    print("Transaksi dicatat di log.");
   }
 }
 
 void main() {
-  final formatRupiah = NumberFormat.currency(
-    locale: 'id_ID',
-    symbol: 'Rp',
-    decimalDigits: 2,
-  );
+  Barang bukuTulis = Barang(nama: 'Buku Tulis', harga: 3000.0, stok: 100);
+  Barang pensil = Barang(nama: 'Pensil', harga: 2000.0, stok: 50);
 
-  BarangPromo Buku = BarangPromo("Buku", 50000.0, 20, 10.0);
-
-  int jumlahbeli = 15;
-  
-  print("=== Struk Belanja ===\n");
-  print("Nama Barang : ${Buku.nama}");
-  print("Stok Barang : ${Buku.stok}");
-  if (Buku.jual(jumlahbeli)) {
-    print("Harga Asli  : Rp${Buku.harga}");
-    print("Diskon      : ${Buku.Diskon}%");
-    print("------------------------------");
-    print("jumlah beli : $jumlahbeli");
-    print("Sisa stok  : ${Buku.stok}");
-    print("HARGA PROMO : Rp${Buku.HargaPromo()}");  
-
-    double totalHarga = Buku.HargaPromo() * jumlahbeli;
-    print("Total Harga : ${formatRupiah.format(totalHarga)}");
-  }
-  print("==============================");
+  prosesBeli("2", bukuTulis); 
+  prosesBeli("dua", pensil);   
 }
-
-// Karena untuk mencegah manipulasi data secara langsung dari luar kelas. Jika stok 
-// tidak dilindungi, programmer lain bisa saja tidak sengaja ataupun dengan sengaja 
-// mengetik kode seperti `buku._stok = -50;` tanpa lewat proses transaksi. 
-// Akibatnya, data di aplikasi menjadi kacau dan tidak sesuai dengan barang
-// yang ada di gudang. Dengan melindunginya, kita memaksa setiap perubahan stok 
-// harus melewati sebuah transaksi yang sudah dilengkapi dengan aturan validasi,
-// sehingga data tetap aman.
